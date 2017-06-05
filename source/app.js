3459054341446cd479d1df2230912c1c1afe6616
magicghost.vu <magicghost.vu@gmail.com>
2017-06-05 23:31:04 +0700

    add import export syntax

--- source/app.js
+++ source/app.js
@@ -1,11 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-class Hello extends React.Component{
+import {variableToExport} from "./module_1";
+import axios from "axios";
+
+class Hello extends React.Component {
     render(){
         return (
-          <h1>Hello world</h1>
+            <div>
+                <h1>Hello world</h1>
+                <h2>Time: {new Date().toLocaleTimeString()}</h2>
+            </div>
         );
     }
 }
 
-ReactDOM.render(<Hello/>, document.getElementById("root"));
+console.log(variableToExport.a);
+let funTick= function () {
+    ReactDOM.render(<Hello/>, document.getElementById("root"));
+};
+
+
+var promiseGet_1= axios({
+    method: 'get',
+    url: "/index.html"
+});
+
+var asyncFunc1= async function () {
+    var response= await promiseGet_1;
+    console.log(response.data)
+};
+
+setInterval(funTick, 1000);
+asyncFunc1();
---
+++ source/module_1.js
@@ -0,0 +1,8 @@
+/**
+ * Created by magic_000 on 05/06/2017.
+ */
+var variableToExport = {
+    a: 12,
+    b:13
+};
+export {variableToExport}
