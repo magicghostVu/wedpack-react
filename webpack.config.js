409f11883f4c4af24d83077faa7ebdb8c1f0806b
DESKTOP-2ACJTHO\Fresher <magicghost.vu@gmail.com>
2017-06-05 19:19:47 +0700

    1st commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: './source/app.js',
+
+    output: {
+        filename: 'bundle.js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+
+};
